9c7482f0816071d4677a520232b25e8404ce23e8
panda <915615059@qq.com>
2024-09-30 15:45:43 +0800

    11

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		Use:   "pginer",
 		Short: "pginer 是一个简单的命令行工具",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello, World1111!")
+			fmt.Println("Hello, World22222!")
 		},
 	}
 
