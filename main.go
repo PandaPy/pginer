2c5f60f7348373835f22bc39244cb1aa6c635341
panda <915615059@qq.com>
2024-09-30 15:33:51 +0800

    11

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package pginer
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "pginer",
+		Short: "pginer 是一个简单的命令行工具",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, World!")
+		},
+	}
+
+	// 执行命令
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
